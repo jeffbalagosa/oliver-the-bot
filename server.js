c0e74c2ba869bd3da539f2073486c019615e9026
Jeff Balagosa <jeff.balagosa@corp.vegas.com>
2023-04-05 11:27:32 -0700

    Refactor CSS and server.

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
-const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 const cors = require("cors");
-const configuration = new Configuration({
-  organization: "org-BoqpqXXbMfa3FW97EToE2H0z",
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const { Configuration, OpenAIApi } = require("openai");
 
 const app = express();
-app.use(bodyParser.json());
+const port = process.env.PORT || 3080;
+
+app.use(json());
 app.use(cors());
 
-const port = 3080;
+app.post("/", handleChatRequest);
 
-// send conversation to bot
-app.post("/", async (req, res) => {
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+async function handleChatRequest(req, res) {
   const { conversation, apiKey } = req.body;
+
   const openaiConfig = new Configuration({
-    organization: "org-BoqpqXXbMfa3FW97EToE2H0z",
+    organization: process.env.OPENAI_ORGANIZATION,
     apiKey: apiKey || process.env.OPENAI_API_KEY,
   });
+
   const openaiInstance = new OpenAIApi(openaiConfig);
   const response = await openaiInstance.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: conversation,
     temperature: 0.6,
   });
+
   res.send({
     message: response.data.choices[0].message.content,
     usage: response.data.usage,
   });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+}
